4f155eba23babdfcbe40fb32f9de0e01d1ba9827
xiemalin <xiemalin@baidu.com>
2022-11-29 14:01:52 +0800

    rename PoolX to Pool

--- poolx/pool.go
+++ poolx/pool.go
@@ -2,14 +2,14 @@ package poolx
 
 import "sync"
 
-type PoolX[E any] struct {
+type Pool[E any] struct {
 	New      func() E
 	internal sync.Pool
 }
 
 // NewPoolX create a new PoolX
-func NewPoolX[E any](f func() E) *PoolX[E] {
-	p := PoolX[E]{New: f}
+func NewPool[E any](f func() E) *Pool[E] {
+	p := Pool[E]{New: f}
 	p.internal = sync.Pool{
 		New: func() any {
 			return p.New()
@@ -20,12 +20,12 @@ func NewPoolX[E any](f func() E) *PoolX[E] {
 }
 
 // Get selects an E generic type item from the Pool
-func (p *PoolX[E]) Get() E {
+func (p *Pool[E]) Get() E {
 	v := p.internal.Get()
 	return v.(E)
 }
 
 // Put adds x to the pool.
-func (p *PoolX[E]) Put(v E) {
+func (p *Pool[E]) Put(v E) {
 	p.internal.Put(v)
 }
---
+++ poolx/pool_test.go
@@ -0,0 +1,32 @@
+package poolx_test
+
+import (
+	"testing"
+
+	"github.com/jhunters/goassist/poolx"
+
+	. "github.com/smartystreets/goconvey/convey"
+)
+
+type Pojo struct {
+	name string
+}
+
+func TestNewPool(t *testing.T) {
+	name1 := "matt"
+	name2 := "matthew"
+	Convey("TestNewPool", t, func() {
+		p := poolx.NewPool(func() *Pojo {
+			return &Pojo{"matt"}
+		})
+
+		p.Put(&Pojo{"matthew"})
+
+		Convey("pool Get test", func() {
+			So(p.Get().name, ShouldEqual, name2) // pop the last put one
+			So(p.Get().name, ShouldEqual, name1) // no element, so New function will be called
+		})
+
+	})
+
+}
