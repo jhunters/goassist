e5df226e805fdfb6425bb8c52a034b14c4f3d30b
xiemalin <xiemalin@baidu.com>
2022-12-14 11:00:02 +0800

    modify test case

--- hashx/hash_test.go
+++ hashx/hash_test.go
@@ -1,33 +1,34 @@
 package hashx_test
 
 import (
-	"fmt"
 	"testing"
 
-	"github.com/jhunters/goassist/conv"
 	"github.com/jhunters/goassist/hashx"
 
 	. "github.com/smartystreets/goconvey/convey"
 )
 
-const (
-	m = 0b1111111111
-)
-
 func TestHashcode(t *testing.T) {
 
-	Convey("TestHashcode", t, func() {
-
+	Convey("HashcodeString", t, func() {
 		hc := hashx.HashcodeString("nodeA")
 		So(hc, ShouldBeGreaterThanOrEqualTo, 0)
-		fmt.Println(hc, conv.FormatInt(hc, 2), hc%m)
 
 		hc = hashx.HashcodeString("nodeB")
 		So(hc, ShouldBeGreaterThanOrEqualTo, 0)
-		fmt.Println(hc, conv.FormatInt(hc, 2), hc%m)
 
 		hc = hashx.HashcodeString("nodeC")
 		So(hc, ShouldBeGreaterThanOrEqualTo, 0)
-		fmt.Println(hc, conv.FormatInt(hc, 2), hc%m)
+	})
+
+	Convey("TestHashcode", t, func() {
+		hc := hashx.Hashcode([]byte("nodeA"))
+		So(hc, ShouldBeGreaterThanOrEqualTo, 0)
+
+		hc = hashx.Hashcode([]byte("nodeB"))
+		So(hc, ShouldBeGreaterThanOrEqualTo, 0)
+
+		hc = hashx.Hashcode([]byte("nodeC"))
+		So(hc, ShouldBeGreaterThanOrEqualTo, 0)
 	})
 }
