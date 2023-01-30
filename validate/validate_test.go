ab07e2aa01f6cb0e777b7aed953b4bcd711582aa
xiemalin <xiemalin@baidu.com>
2023-01-30 11:28:38 +0800

    fix test case

--- validate/validate_test.go
+++ validate/validate_test.go
@@ -1,13 +1,8 @@
 package validate_test
 
 import (
-	"fmt"
-	"io/ioutil"
-	"regexp"
-	"strings"
 	"testing"
 
-	"github.com/jhunters/goassist/stringutil"
 	"github.com/jhunters/goassist/validate"
 	. "github.com/smartystreets/goconvey/convey"
 )
@@ -43,41 +38,4 @@ func TestValidateEmail(t *testing.T) {
 		})
 
 	})
-}
-
-func TestXxx(t *testing.T) {
-
-	s := "第一百四十九章 尤梦清落败"
-
-	reg, _ := regexp.Compile(`(第[\W]+章)\s+([\w|\W]+)`)
-	b := reg.MatchString(s)
-	fmt.Println(b)
-
-	result := reg.FindAllStringSubmatch(s, -1)
-	if len(result) == 1 && len(result[0]) == 2 {
-		fmt.Println(result[0][0], result[0][1])
-	}
-
-}
-
-func TestGetPrefaceAndName(t *testing.T) {
-
-	reg, _ := regexp.Compile(`(第[\d]+章)\s+([\w|\W]+)`)
-
-	c, err := ioutil.ReadFile(`D:\documents\medicine.txt`)
-	if err != nil {
-		t.Error(err)
-		return
-	}
-
-	lines := strings.Split(string(c), "\n")
-	for i := 0; i < len(lines); i++ {
-		line := lines[i]
-		if reg.MatchString(line) {
-			if strings.Contains(line, "（") {
-				line = stringutil.SubstringBefore(line, "（")
-			}
-			fmt.Println(line)
-		}
-	}
 }
