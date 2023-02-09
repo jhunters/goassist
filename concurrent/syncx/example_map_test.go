41448381770d831bde417f75988b8d7365bd265f
xiemalin <xiemalin@baidu.com>
2023-02-09 11:16:27 +0800

    add example code

--- concurrent/syncx/example_map_test.go
+++ concurrent/syncx/example_map_test.go
@@ -35,10 +35,23 @@ func ExampleNewMapByInitial() {
 	fmt.Println(mp.Exist("key1"))
 	fmt.Println(mp.ExistValue(newMapExamplePojo("world")))
 
+	// LoadOrStore
+	v, loaded := mp.LoadOrStore("key1", newMapExamplePojo("value1"))
+	fmt.Println(v.Name, loaded)
+	v, loaded = mp.LoadOrStore("key3", newMapExamplePojo("value3"))
+	fmt.Println(v.Name, loaded)
+
+	// LoadAndDelete
+	v, loaded = mp.LoadAndDelete("key3")
+	fmt.Println(v.Name, loaded)
+
 	// Output:
 	// 2
 	// true
 	// key2 true
+	// hello true
+	// value3 false
+	// value3 true
 }
 
 func ExampleNewMap() {
