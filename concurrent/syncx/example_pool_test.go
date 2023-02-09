4bf19a271071901a4701f4d46684fa5ae0e80e13
xiemalin <xiemalin@baidu.com>
2023-02-09 11:11:39 +0800

    remove unused code

--- concurrent/syncx/example_pool_test.go
+++ concurrent/syncx/example_pool_test.go
@@ -2,9 +2,6 @@ package syncx_test
 
 import (
 	"fmt"
-	"sync"
-	"testing"
-	"time"
 
 	"github.com/jhunters/goassist/concurrent/syncx"
 )
@@ -32,47 +29,4 @@ func ExampleNewPool() {
 	// matthew
 	// matt
 	// matthew
-}
-
-func TestCond(t *testing.T) {
-	var m sync.Mutex
-	c := sync.NewCond(&m)
-
-	for i := 0; i < 10; i++ {
-		c.L.Lock()
-		go func(c *sync.Cond, no int) {
-			c.Wait()
-			fmt.Println("no=", no, "finished")
-			c.L.Unlock()
-		}(c, i)
-	}
-
-	for i := 0; i < 10; i++ {
-		time.Sleep(time.Second)
-		c.Signal()
-	}
-
-}
-
-func TestWait(t *testing.T) {
-	var wg sync.WaitGroup
-	var urls = []string{
-		"http://www.golang.org/",
-		"http://www.google.com/",
-		"http://www.example.com/",
-	}
-	for _, url := range urls {
-		// Increment the WaitGroup counter.
-		wg.Add(1)
-		// Launch a goroutine to fetch the URL.
-		go func(url string) {
-			// Decrement the counter when the goroutine completes.
-			defer wg.Done()
-			// Fetch the URL.
-			fmt.Println(url)
-			fmt.Println("done==>", url)
-		}(url)
-	}
-	// Wait for all HTTP fetches to complete.
-	wg.Wait()
 }
