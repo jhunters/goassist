557c72f3dcbfc7159d75836e97a9760af83eba6b
xiemalin <xiemalin@baidu.com>
2023-02-23 19:10:14 +0800

    add ByteBuffer enhanced for  bytes.Buffer

---
+++ bytex/bytebuff.go
@@ -0,0 +1,120 @@
+package bytex
+
+import (
+	"bytes"
+	"fmt"
+
+	"github.com/jhunters/goassist/arrayutil"
+)
+
+type ByteBuffer struct {
+	bytes.Buffer
+}
+
+// NewByteBuffer return a new ByteBuffer
+func NewByteBuffer(bb []byte) *ByteBuffer {
+	ret := &ByteBuffer{}
+	ret.reset(bb)
+	return ret
+}
+
+// Delete some bytes between begin and end index. note: this operation will reset offset
+func (bbuf *ByteBuffer) Delete(begin, end int) {
+	if begin < 0 || begin > bbuf.Len() || begin > end {
+		return
+	}
+	if end > bbuf.Len() {
+		bb := bbuf.Bytes()[:begin]
+		bbuf.reset(bb)
+		return
+	}
+
+	bb := bbuf.Bytes()[:begin]
+	ret := make([]byte, len(bb))
+	copy(ret, bb)
+	ret = append(ret, bbuf.Bytes()[end:]...)
+	bbuf.reset(ret)
+}
+
+// ReplaceByOffset to replace sub slice by offset index. note: this operation will reset offset
+func (bbuf *ByteBuffer) ReplaceByOffset(begin, end int, sub []byte) (err error) {
+	if begin > bbuf.Len() || end > bbuf.Len() {
+		err = fmt.Errorf("out of index. begin=%d, end=%d", begin, end)
+		return
+	}
+	if begin > end {
+		err = fmt.Errorf("begin should lower than end. begin=%d, end=%d", begin, end)
+		return
+	}
+
+	bb := bbuf.Bytes()
+	b1 := bb[:begin]
+	ret := make([]byte, len(b1))
+	copy(ret, b1)
+	ret = append(ret, sub...)
+	ret = append(ret, bb[end:]...)
+
+	return bbuf.reset(ret)
+}
+
+func (bbuf *ByteBuffer) reset(value []byte) error {
+	bbuf.Reset()
+	bbuf.Grow(len(value))
+	_, err := bbuf.Write(value)
+	return err
+}
+
+// Index returns the index of the first instance of sub in s, or -1 if sub is not present in s. note: this operation will reset offset
+func (bbuf *ByteBuffer) Index(sub []byte) int {
+	return bytes.Index(bbuf.Bytes(), sub)
+}
+
+// Index returns the index of the fromIndex index instance of sub in s, or -1 if sub is not present in s. note: this operation will reset offset
+func (bbuf *ByteBuffer) IndexOffset(sub []byte, fromIndex int) int {
+	if fromIndex < 0 || fromIndex+len(sub) >= bbuf.Len() {
+		return -1
+	}
+	left, _ := bbuf.SubBytes(fromIndex, bbuf.Len())
+	index := bytes.Index(left, sub)
+	if index != -1 {
+		index += fromIndex
+	}
+	return index
+}
+
+// Insert the sub slice into the target offset.note: this operation will reset offset
+func (bbuf *ByteBuffer) Insert(offset int, sub []byte) (err error) {
+	if offset < 0 || offset > bbuf.Len() {
+		return fmt.Errorf("out of index, offset=%d", offset)
+	}
+
+	if offset == bbuf.Len() {
+		return bbuf.reset(append(bbuf.Bytes(), sub...))
+	}
+
+	bb := bbuf.Bytes()
+	b1 := bb[:offset]
+	ret := make([]byte, len(b1))
+	copy(ret, b1)
+	ret = append(ret, sub...)
+	ret = append(ret, bb[offset:]...)
+	return bbuf.reset(ret)
+}
+
+// SubString a string that is a substring of this string.note: this operation will reset offset
+func (bbuf *ByteBuffer) SubBytes(beginIndex, endIndex int) ([]byte, error) {
+	if beginIndex < 0 || beginIndex > bbuf.Len() {
+		return nil, fmt.Errorf("out of index")
+	}
+	if endIndex > bbuf.Len() || endIndex < 0 {
+		return bbuf.Bytes()[beginIndex:], nil
+	}
+
+	return bbuf.Bytes()[beginIndex:endIndex], nil
+}
+
+// Reverse the order to slice
+func (bbuf *ByteBuffer) Reverse() {
+	bb := bbuf.Bytes()
+	arrayutil.Reverse(bb)
+}
---
+++ bytex/bytebuff_test.go
@@ -0,0 +1,191 @@
+package bytex_test
+
+import (
+	"fmt"
+	"testing"
+
+	"github.com/jhunters/goassist/bytex"
+	. "github.com/smartystreets/goconvey/convey"
+)
+
+func TestReplaceByOffset(t *testing.T) {
+	Convey("TestReplaceByOffset", t, func() {
+		bbuf := bytex.NewByteBuffer([]byte("hello world"))
+		err := bbuf.ReplaceByOffset(1, 2, []byte("yes"))
+		So(err, ShouldBeNil)
+		So(bbuf.Bytes(), ShouldResemble, []byte("hyesllo world"))
+	})
+
+	Convey("TestReplaceByOffset outof index", t, func() {
+		bbuf := bytex.NewByteBuffer([]byte("hello world"))
+		err := bbuf.ReplaceByOffset(2, 1, []byte("yes"))
+		So(err, ShouldNotBeNil)
+
+		err = bbuf.ReplaceByOffset(2, 100, []byte("yes"))
+		So(err, ShouldNotBeNil)
+	})
+
+}
+
+func TestInsert(t *testing.T) {
+
+	Convey("TestInsert start", t, func() {
+		bbuf := bytex.NewByteBuffer([]byte("hello world"))
+		err := bbuf.Insert(0, []byte("yes"))
+		So(err, ShouldBeNil)
+		So(bbuf.String(), ShouldEqual, "yeshello world")
+
+	})
+
+	Convey("TestInsert end", t, func() {
+		bbuf := bytex.NewByteBuffer([]byte("hello world"))
+		err := bbuf.Insert(bbuf.Len(), []byte("yes"))
+		So(err, ShouldBeNil)
+		So(bbuf.String(), ShouldEqual, "hello worldyes")
+
+	})
+
+	Convey("TestInsert middle position", t, func() {
+		bbuf := bytex.NewByteBuffer([]byte("hello world"))
+		err := bbuf.Insert(3, []byte("yes"))
+		So(err, ShouldBeNil)
+		So(bbuf.String(), ShouldEqual, "helyeslo world")
+
+	})
+
+	Convey("TestInsert out of index", t, func() {
+		bbuf := bytex.NewByteBuffer([]byte("hello world"))
+		err := bbuf.Insert(bbuf.Len()+1, []byte("yes"))
+		So(err, ShouldNotBeNil)
+	})
+
+}
+
+func TestIndex(t *testing.T) {
+	Convey("TestIndex", t, func() {
+
+		bbuf := bytex.NewByteBuffer([]byte("hello world"))
+		idx := bbuf.Index([]byte("lo wo"))
+		So(idx, ShouldEqual, 3)
+
+		// test not found
+		idx = bbuf.Index([]byte("lo wol"))
+		So(idx, ShouldEqual, -1)
+
+	})
+
+}
+
+func TestSubBytes(t *testing.T) {
+	Convey("TestSubBytes", t, func() {
+		origin := []byte("hello world")
+		bbuf := bytex.NewByteBuffer(origin)
+
+		// sub all
+		bb, err := bbuf.SubBytes(0, bbuf.Len())
+		So(err, ShouldBeNil)
+		So(bb, ShouldResemble, bbuf.Bytes())
+
+		bb, err = bbuf.SubBytes(0, 50)
+		So(err, ShouldBeNil)
+		So(bb, ShouldResemble, bbuf.Bytes())
+
+		// sub partial
+		bb, err = bbuf.SubBytes(0, 5)
+		So(err, ShouldBeNil)
+		So(bb, ShouldResemble, origin[:5])
+
+		// out of index
+		bb, err = bbuf.SubBytes(25, 50)
+		So(err, ShouldNotBeNil)
+		So(bb, ShouldBeNil)
+	})
+
+}
+
+func TestDelete(t *testing.T) {
+	Convey("TestDelete", t, func() {
+		origin := []byte("hello world")
+		bbuf := bytex.NewByteBuffer(origin)
+
+		// test delete all
+		bbuf.Delete(0, bbuf.Len())
+		So(bbuf.Bytes(), ShouldBeEmpty)
+
+		bbuf = bytex.NewByteBuffer(origin)
+		// test detele start
+		bbuf.Delete(0, 5)
+		So(bbuf.String(), ShouldEqual, " world")
+
+		// test delete end with
+		bbuf = bytex.NewByteBuffer(origin)
+		bbuf.Delete(5, 15)
+		So(bbuf.String(), ShouldEqual, "hello")
+
+		// test delete out of index
+		bbuf = bytex.NewByteBuffer(origin)
+		bbuf.Delete(-1, 15)
+
+	})
+
+}
+
+func TestReverse(t *testing.T) {
+	Convey("TestReverse", t, func() {
+		origin := []byte("hello world")
+		bbuf := bytex.NewByteBuffer(origin)
+
+		bbuf.Reverse()
+
+		So(bbuf.Bytes(), ShouldResemble, []byte("dlrow olleh"))
+	})
+
+}
+
+func ExampleNewByteBuffer() {
+	origin := []byte("hello world")
+	bbuf := bytex.NewByteBuffer(origin)
+	err := bbuf.ReplaceByOffset(1, 2, []byte("yes"))
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+	fmt.Println(bbuf)
+
+	// insert
+	err = bbuf.Insert(1, []byte("test"))
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+	fmt.Println(bbuf)
+
+	// index
+	idx := bbuf.Index([]byte("lo wo"))
+	fmt.Println(idx)
+
+	// SubBytes
+	bb, err := bbuf.SubBytes(2, 5)
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+	fmt.Println(bb)
+
+	bbuf = bytex.NewByteBuffer(origin)
+	// test detele start
+	bbuf.Delete(0, 6)
+	fmt.Println(bbuf)
+
+	bbuf = bytex.NewByteBuffer(origin)
+	bbuf.Reverse()
+	fmt.Println(bbuf)
+
+	// Output:
+	// hyesllo world
+	// htestyesllo world
+	// 9
+	// [101 115 116]
+	// world
+	// dlrow olleh
+}
