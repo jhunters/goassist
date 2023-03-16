8126600a061247cb985e3cf8b88cfb5d01ced5a4
xiemalin <xiemalin@baidu.com>
2023-03-16 15:52:36 +0800

    add example codes

--- concurrent/chans_test.go
+++ concurrent/chans_test.go
@@ -1,6 +1,7 @@
 package concurrent_test
 
 import (
+	"fmt"
 	"testing"
 	"time"
 
@@ -43,7 +44,7 @@ func TestTrySend(t *testing.T) {
 		})
 
 		Convey("TestTrySend with closed channel", func() {
-			ch := make(chan string, 0)
+			ch := make(chan string)
 			concurrent.SafeCloseChan(ch)
 			ok := concurrent.TrySendChan(s, ch, 1*time.Second)
 			So(ok, ShouldBeFalse)
@@ -53,17 +54,17 @@ func TestTrySend(t *testing.T) {
 			ch := make(chan string, 1)
 			now := time.Now()
 			ok := concurrent.TrySendChan(s, ch, 1*time.Second)
-			took := time.Now().Sub(now)
+			took := time.Since(now)
 			So(ok, ShouldBeTrue)
 			So(took, ShouldBeLessThan, 1*time.Second)
 			So(s, ShouldEqual, <-ch)
 		})
 
 		Convey("TestTrySend out of time", func() {
-			ch := make(chan string, 0)
+			ch := make(chan string)
 			now := time.Now()
 			ok := concurrent.TrySendChan(s, ch, 1*time.Second)
-			took := time.Now().Sub(now)
+			took := time.Since(now)
 			So(ok, ShouldBeFalse)
 			So(took, ShouldBeGreaterThanOrEqualTo, 1*time.Second)
 			So(took, ShouldBeLessThan, 2*time.Second)
@@ -87,6 +88,36 @@ func TestTrySend(t *testing.T) {
 
 }
 
+func ExampleTrySendChan() {
+	s := "hello"
+
+	// TrySend to nil channel
+	var ch chan string = nil
+	ok := concurrent.TrySendChan(s, ch, 1*time.Second)
+	fmt.Println(ok)
+
+	// TrySend to closed channel
+	ch = make(chan string)
+	concurrent.SafeCloseChan(ch)
+	ok = concurrent.TrySendChan(s, ch, 1*time.Second)
+	fmt.Println(ok)
+
+	// TrySend to no buffered channel and occures time out
+	ch = make(chan string)
+	ok = concurrent.TrySendChan(s, ch, 1*time.Second)
+	fmt.Println(ok)
+
+	ch = make(chan string, 1)
+	ok = concurrent.TrySendChan(s, ch, 1*time.Second)
+	fmt.Println(ok)
+
+	// Output:
+	// false
+	// false
+	// false
+	// true
+}
+
 func TestTryReceive(t *testing.T) {
 	Convey("TestTryReceive", t, func() {
 		s := "hello"
@@ -98,7 +129,7 @@ func TestTryReceive(t *testing.T) {
 		})
 
 		Convey("TestTryReceive with closed channel", func() {
-			ch := make(chan string, 0)
+			ch := make(chan string)
 			concurrent.SafeCloseChan(ch)
 			ok, v := concurrent.TryRecevieChan(ch, 1*time.Second)
 			So(ok, ShouldBeTrue)
@@ -110,7 +141,7 @@ func TestTryReceive(t *testing.T) {
 			ch <- s
 			now := time.Now()
 			ok, f := concurrent.TryRecevieChan(ch, 1*time.Second)
-			took := time.Now().Sub(now)
+			took := time.Since(now)
 			So(ok, ShouldBeTrue)
 			So(s, ShouldEqual, f)
 			So(took, ShouldBeLessThan, 1*time.Second)
@@ -120,7 +151,7 @@ func TestTryReceive(t *testing.T) {
 			ch := make(chan string, 1)
 			now := time.Now()
 			ok, _ := concurrent.TryRecevieChan(ch, 1*time.Second)
-			took := time.Now().Sub(now)
+			took := time.Since(now)
 			So(ok, ShouldBeFalse)
 			So(took, ShouldBeGreaterThanOrEqualTo, 1*time.Second)
 			So(took, ShouldBeLessThan, 2*time.Second)
@@ -141,4 +172,37 @@ func TestTryReceive(t *testing.T) {
 		})
 	})
 
+}
+
+func ExampleTryRecevieChan() {
+
+	// TryReceive from nil channel
+	var ch chan string = nil
+	ok, _ := concurrent.TryRecevieChan(ch, 1*time.Second)
+	fmt.Println(ok)
+
+	// TryReceive from closed channel
+	ch = make(chan string)
+	concurrent.SafeCloseChan(ch)
+	ok, v := concurrent.TryRecevieChan(ch, 1*time.Second)
+	fmt.Println(ok)
+	fmt.Println(v) // empty value
+
+	// TryReceive in time
+	ch = make(chan string, 1)
+	ch <- "hello"
+	ok, v = concurrent.TryRecevieChan(ch, 1*time.Second)
+	fmt.Println(ok, v)
+
+	// TryReceive from no value channel and occures time out
+	ch = make(chan string, 1)
+	ok, _ = concurrent.TryRecevieChan(ch, 1*time.Second)
+	fmt.Println(ok)
+
+	// Output:
+	// false
+	// true
+	//
+	// true hello
+	// false
 }
