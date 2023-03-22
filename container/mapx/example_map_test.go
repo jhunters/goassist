a7c7dc16248c45e31a9a8c1a298c709f014844a9
xiemalin <xiemalin@baidu.com>
2023-03-22 18:02:03 +0800

    add example code

--- container/mapx/example_map_test.go
+++ container/mapx/example_map_test.go
@@ -73,3 +73,163 @@ func ExampleNewMap() {
 	// key3 true
 	// true
 }
+
+func ExampleMap_IsEmpty() {
+	mp := mapx.NewMap[string, string]()
+	fmt.Println(mp.IsEmpty())
+
+	// Output:
+	// true
+}
+
+func ExampleMap_Equals() {
+	mp1 := mapx.NewMap[string, string]()
+	mp1.Put("key1", "value1")
+	mp2 := mapx.NewMap[string, string]()
+	mp2.Put("key1", "value1")
+
+	fmt.Println(mp1.Equals(mp2, func(s1, s2 string) bool { return s1 == s2 }))
+}
+
+func ExampleMap_MaxKey() {
+	mp := mapx.NewMap[string, string]()
+	mp.Put("key1", ("2"))
+	mp.Put("key2", ("3"))
+	mp.Put("key3", ("4"))
+	mp.Put("key4", ("1"))
+	mp.Put("key5", ("5"))
+
+	k, v := mp.MaxKey(func(s1, s2 string) int { return strings.Compare(s1, s2) })
+	fmt.Println(k, v)
+
+	// Output:
+	// key5 5
+}
+
+func ExampleMap_MinKey() {
+	mp := mapx.NewMap[string, string]()
+	mp.Put("key1", ("2"))
+	mp.Put("key2", ("3"))
+	mp.Put("key3", ("4"))
+	mp.Put("key4", ("1"))
+	mp.Put("key5", ("5"))
+
+	k, v := mp.MinKey(func(s1, s2 string) int { return strings.Compare(s1, s2) })
+	fmt.Println(k, v)
+
+	// Output:
+	// key1 2
+}
+
+func ExampleMap_MinValue() {
+	mp := mapx.NewMap[string, string]()
+	mp.Put("key1", ("2"))
+	mp.Put("key2", ("3"))
+	mp.Put("key3", ("4"))
+	mp.Put("key4", ("1"))
+	mp.Put("key5", ("5"))
+
+	k, v := mp.MinValue(func(s1, s2 string) int { return strings.Compare(s1, s2) })
+	fmt.Println(k, v)
+
+	// Output:
+	// key4 1
+}
+
+func ExampleMap_MaxValue() {
+	mp := mapx.NewMap[string, string]()
+	mp.Put("key1", ("2"))
+	mp.Put("key2", ("3"))
+	mp.Put("key3", ("4"))
+	mp.Put("key4", ("1"))
+	mp.Put("key5", ("5"))
+
+	k, v := mp.MaxValue(func(s1, s2 string) int { return strings.Compare(s1, s2) })
+	fmt.Println(k, v)
+
+	// Output:
+	// key5 5
+}
+
+func ExampleMap_Exist() {
+	mp := mapx.NewMap[string, string]()
+	mp.Put("key1", ("2"))
+	mp.Put("key2", ("3"))
+	mp.Put("key3", ("4"))
+	mp.Put("key4", ("1"))
+	mp.Put("key5", ("5"))
+
+	b := mp.Exist("key2")
+	fmt.Println(b)
+
+	// Output:
+	// true
+}
+
+func ExampleMap_ExistValue() {
+	mp := mapx.NewMap[string, string]()
+	mp.Put("key1", ("2"))
+	mp.Put("key2", ("3"))
+	mp.Put("key3", ("4"))
+	mp.Put("key4", ("1"))
+	mp.Put("key5", ("5"))
+
+	k, exist := mp.ExistValue("5")
+	fmt.Println(k, exist)
+
+	k, exist = mp.ExistValue("15")
+	fmt.Println(k, exist)
+
+	// Output:
+	// key5 true
+	//  false
+}
+
+func ExampleMap_ExistValueWithComparator() {
+	mp := mapx.NewMap[string, string]()
+	mp.Put("key1", ("2"))
+	mp.Put("key2", ("3"))
+	mp.Put("key3", ("4"))
+	mp.Put("key4", ("1"))
+	mp.Put("key5", ("5"))
+
+	k, exist := mp.ExistValueWithComparator("5", func(s1, s2 string) bool { return s1 == s2 })
+	fmt.Println(k, exist)
+
+	k, exist = mp.ExistValueWithComparator("15", func(s1, s2 string) bool { return s1 == s2 })
+	fmt.Println(k, exist)
+
+	// Output:
+	// key5 true
+	//  false
+}
+
+func ExampleMap_Keys() {
+	mp := mapx.NewMap[string, string]()
+	mp.Put("key1", ("2"))
+	mp.Put("key2", ("3"))
+	mp.Put("key3", ("4"))
+	mp.Put("key4", ("1"))
+	mp.Put("key5", ("5"))
+
+	keys := mp.Keys()
+	fmt.Println(len(keys))
+
+	// Output:
+	// 5
+}
+
+func ExampleMap_Values() {
+	mp := mapx.NewMap[string, string]()
+	mp.Put("key1", ("2"))
+	mp.Put("key2", ("3"))
+	mp.Put("key3", ("4"))
+	mp.Put("key4", ("1"))
+	mp.Put("key5", ("5"))
+
+	vals := mp.Values()
+	fmt.Println(len(vals))
+
+	// Output:
+	// 5
+}
