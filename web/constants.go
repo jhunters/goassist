751db1903e3b2406c4a33c4edd2c7b18f671b8b1
xiemalin <xiemalin@baidu.com>
2024-04-22 17:19:36 +0800

    add constants

---
+++ web/constants.go
@@ -0,0 +1,17 @@
+package web
+
+const (
+	HTTP_HEADER_CONTENT_TYPE      = "Content-Type"
+	HTTP_HEADER_CONTENT_TYPE_JSON = "application/json"
+	HTTP_HEADER_CONTENT_TYPE_TEXT = "text/plain"
+	HTTP_HEADER_CONTENT_TYPE_HTML = "text/html"
+	HTTP_HEADER_CONTENT_TYPE_ES   = "text/event-stream"
+
+	HTTP_HEADER_CACHE_CONTROL            = "Cache-Control"
+	HTTP_HEADER_CONTENT_Control_NO_CACHE = "no-cache"
+
+	HTTP_HEADER_CONNECTION = "Connection"
+	HTTP_HEADER_KEEPALIVE  = "keep-alive"
+
+	HTTP_HEADER_ACCESS_CONTROL_ALLOW_ORIGIN = "Access-Control-Allow-Origin"
+)
