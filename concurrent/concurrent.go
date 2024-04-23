f1f3122254183aab7778a64b99a08014849c8599
xiemalin <xiemalin@baidu.com>
2024-04-23 11:25:34 +0800

    add comments

--- concurrent/concurrent.go
+++ concurrent/concurrent.go
@@ -63,9 +63,17 @@ func AsyncGo(f base.Call, timeout time.Duration) (ok bool, err error) {
 
 }
 
-// AsyncGo execute target function by goroutine. if panic happened will wrap error object and return false
-// if just time out will return ok(false), err(nil)
-func AsyncGoWithEvent[E any](f base.Call, toevent <-chan E) (ok bool, err error) {
+// AsyncGoWithEvent 是一个异步执行的函数，它接受一个函数f和一个事件通道toevent作为参数
+// f函数将在新的goroutine中执行，如果f函数执行成功，则ch通道关闭
+// 如果在f函数执行前，toevent通道接收到事件，则函数返回false
+// 函数返回两个值，ok表示f函数是否执行成功，err表示f函数执行时产生的错误
+// 参数：
+// f：要执行的函数
+// cancal：事件通道
+// 返回值：
+// ok：f函数是否执行成功
+// err：f函数执行时产生的错误
+func AsyncGoWithEvent[E any](f base.Call, cancal <-chan E) (ok bool, err error) {
 	ch := make(chan error, 1)
 	go func(ch chan<- error) {
 		defer panicCatch(ch)
@@ -77,7 +85,7 @@ func AsyncGoWithEvent[E any](f base.Call, toevent <-chan E) (ok bool, err error)
 	case e := <-ch:
 		ok = (e == nil)
 		err = e
-	case <-toevent:
+	case <-cancal:
 		ok = false
 	}
 	return
@@ -102,7 +110,7 @@ func AsyncCall[E any](f base.Supplier[E], timeout time.Duration) (future base.Su
 
 // AsyncCall execute target function by goroutine and has a generic returned parameter. if panic happened will wrap error object and return future(nil)
 // if just time out will return future(func), err(nil)
-func AsyncCallWithEvent[E, T any](f base.Supplier[E], toevent <-chan T) (future base.Supplier[E], err error) {
+func AsyncCallWithEvent[E, T any](f base.Supplier[E], cancal <-chan T) (future base.Supplier[E], err error) {
 	ret := make(chan E, 1)
 	future = func() E {
 		return <-ret
@@ -121,7 +129,7 @@ func AsyncCallWithEvent[E, T any](f base.Supplier[E], toevent <-chan T) (future 
 		if e != nil {
 			future = nil
 		}
-	case <-toevent:
+	case <-cancal:
 		err = fmt.Errorf("AsyncCall execute timeout")
 	}
 	return
