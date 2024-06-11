83391905b27020bdba3f9142a9c3f05439eca479
xiemalin <xiemalin@baidu.com>
2024-06-11 14:39:36 +0800

    add json encode & decode

--- maputil/map.go
+++ maputil/map.go
@@ -3,6 +3,11 @@
  */
 package maputil
 
+import (
+	"bytes"
+	"encoding/json"
+)
+
 // Clone copy all key and value to a new map
 func Clone[E comparable, V any](mapa map[E]V) map[E]V {
 	size := len(mapa)
@@ -34,4 +39,25 @@ func Clear[E comparable, V any](mapa map[E]V) {
 	for k := range mapa {
 		delete(mapa, k)
 	}
+}
+
+// JsonEncode 函数将一个类型为 map[E]V 的字典转换为 JSON 格式的字节数组
+// 其中 E 必须是可比较的类型，V 可以是任意类型
+// 参数 mapa 是待转换的字典
+// 返回值为转换后的 JSON 格式的字节数组以及可能出现的错误
+func JsonEncode[E comparable, V any](mapa map[E]V) ([]byte, error) {
+	buf := new(bytes.Buffer)
+	err := json.NewEncoder(buf).Encode(mapa)
+	return buf.Bytes(), err
+}
+
+// JsonDecode 函数接受一个字节切片作为参数，用于解码JSON格式的数据，
+// 并将解码后的结果存储在一个以E类型为键，V类型为值的map中返回。
+// 如果解码失败，则返回error类型的错误信息。
+// 其中，E类型需要是可比较的，V类型则没有限制。
+func JsonDecode[E comparable, V any](data []byte) (map[E]V, error) {
+	buf := bytes.NewBuffer(data)
+	ret := make(map[E]V)
+	err := json.NewDecoder(buf).Decode(&ret)
+	return ret, err
 }
